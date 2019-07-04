e8cb6008507a08211a656877ac13319a6e22c4f5
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-07-03 22:14:04 -0400

    added Jumbotron component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Donations from "./pages/Donations";
 import LogIn from "./pages/LogIn";
 import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
 import About from "./pages/About";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Router>
       <div>
         <Navbar />
+        <Jumbotron/>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/home" component={Home} />
