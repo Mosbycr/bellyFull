c9586850e4b6af44faf3445b273e0c5ba78cd796
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-07-05 18:15:03 -0400

    added tag line

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -4,8 +4,8 @@ function Jumbotron (){
         <div
         style={{ background: "grey", height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
       className="jumbotron">
-  <h1 className="display-4">Hello, world!</h1>
-  <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <h1 className="display-4">Feeding Richmond Since 2019</h1>
+  {/* <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p> */}
   </div>
         
     );
