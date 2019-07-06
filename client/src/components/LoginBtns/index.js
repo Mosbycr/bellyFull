711bbdddd7157b97ae891c13f566ffa4a2fcd098
Mosbycr <mosbycr@gmail.com>
2019-07-06 18:43:34 -0400

    added buttons for signin and register in loginbtn component

--- client/src/components/LoginBtns/index.js
+++ client/src/components/LoginBtns/index.js
@@ -1,12 +1,32 @@
 import React, {Component} from "react";
 
 class LoginBtns extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signIn: false,
+      signUp: false,
+    };
+    // this.handleSavedBook = this.handleSavedBook.bind(this);
+  }
 
-    render(){
-        return (
-            <div>This is working as sign in button section!</div>
-        )
-    }
+  handleSignInClick = event => {
+      alert("This button clicks!")
+    //   this.setState({signIn: true});
+  }
+
+  render() {
+    return (
+        <div className="container-fluid text-center">
+            <div className="row">
+                <div className="col-12">
+                    <button className="btn" onClick={this.handleSignInClick}>Sign In</button>
+                    <button className="btn">Register</button>
+                </div>
+            </div>
+        </div>
+    )
+  }
 }
 
 export default LoginBtns
