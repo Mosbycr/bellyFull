2d65b4445e09d85a71d96d8688e300a70b841e82
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-07-06 14:00:50 -0400

    created the table/grid on donations page with headers

---
+++ client/src/components/DonationsCard/DonationsCard.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import "./style.css";
+
+// function DonationsCard(){
+//     return(
+        
+//     )
+// }
+// export default DonationsCard;
--- client/src/pages/Donations.js
+++ client/src/pages/Donations.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+// import DonationsCard from "../components/DonationsCard/DonationsCard";
 
-class Donations extends Component {
-  render() {
-    return <div>Hello</div>;
-  }
+function Donations (){
+  return(
+    <div className= "container-fluid">
+     <div className= "row">
+       <div className="col-md-6 text-center">
+         <h2>Available Donations</h2>
+       </div>
+       <div className = "col-md-6 text-center">
+         <h2>Claimed</h2>
+       </div>
+    </div>
+    </div>
+  )
 }
 
 export default Donations;
