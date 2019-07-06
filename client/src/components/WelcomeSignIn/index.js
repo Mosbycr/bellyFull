2539c4133272ae5ab0cc529b83a67ff5449ac8b2
Mosbycr <mosbycr@gmail.com>
2019-07-06 13:56:01 -0400

    added text to welcomesignin component

--- client/src/components/WelcomeSignIn/index.js
+++ client/src/components/WelcomeSignIn/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 function Welcome(){
-    return(
-        <div>This is working!</div>
-    )
+    return (
+      <div className="signinWelcome text-center">
+        <h1 className="signinHeader">Welcome to Bellyfull!</h1>
+        <h2 className="secondHeader">
+          Let's fight hunger and fill some bellies
+        </h2>
+        <p className="welcomeMsg">
+          Sign in to connect with local restaurants. New here? Register your
+          organization and start connecting today.
+        </p>
+      </div>
+    );
 }
 
 export default Welcome
