22173dec5b8fd957421e85233f0b3838f8e105af
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-07-07 14:53:46 -0400

    ClaimBtn is clickable

--- client/src/components/ClaimBtn/ClaimBtn.js
+++ client/src/components/ClaimBtn/ClaimBtn.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, {Component} from "react";
 import "./style.css";
 
-function ClaimBtn(props) {
-  return (
-    <button onClick={props.onClick} className={`card-btn ${props["data-value"]}`} {...props}>
-        Claim
-    </button>
-  );
+class ClaimBtn extends Component{
+
+   handleClaimClick = event =>{
+       alert("Btn was clicked!!")
+   }
+
+    render(){
+        return(
+            <button className = "btn btn-primary " onClick ={this.handleClaimClick}>Claim</button>
+
+
+        )
+
+    
+
 }
+
+}
+
+
+
+// function ClaimBtn(props) {
+//   return (
+//     <button onClick={props.onClick} className={`card-btn ${props["data-value"]}`} {...props}>
+//         Claim
+//     </button>
+//   );
+// }
 
 export default ClaimBtn;
