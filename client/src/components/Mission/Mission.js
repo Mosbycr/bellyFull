90beb7de2e700fe989064e641414ed07d50fe825
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-07-09 19:51:39 -0400

    added get started button

---
+++ client/src/components/Mission/Mission.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Mission.css";
+function Mission() {
+  return (
+    <div class="row">
+      <div class="col-sm-12">
+    
+          <p>
+            At BellyFull our vision is a simple one, help the Richmond
+            community. As residents of a food desert we believe it is our
+            responsibility to help those who may not have access to fresh food
+            daily. By connection restauranteurs with non-profits in the area we
+            hope to reduce food waste as well as provide food to those who are
+            food insecure.
+          </p>
+          <a href= "/LogIn" className = "btn btn-primary ">Get Started</a>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Mission;
