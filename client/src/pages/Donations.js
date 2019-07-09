647cf9275073f164148715f88ba8666f36451760
Mosbycr <mosbycr@gmail.com>
2019-07-09 19:57:49 -0400

    removed component from react import

--- client/src/pages/Donations.js
+++ client/src/pages/Donations.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import DonationsCard from "../components/DonationsCard/DonationsCard";
 import ClaimedCard from "../components/ClaimedCard/ClaimedCard";
 
