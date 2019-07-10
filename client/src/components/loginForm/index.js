f46422f5b9fbbdfb740cb421a891bee729367193
Mosbycr <mosbycr@gmail.com>
2019-07-09 20:39:23 -0400

    added some Yup validation to forms, npm installed and imported Yup

--- client/src/components/loginForm/index.js
+++ client/src/components/loginForm/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Form, Input} from "@rocketseat/unform";
 import "./index.css";
+import * as Yup from "yup";
 
 function LogInForm(props){
     function handleSubmit(data){
@@ -10,34 +11,83 @@ function LogInForm(props){
         console.log(email);
     }
 
+    const schema = Yup.object().shape({
+        email: Yup.string().email("Please enter a valid email")
+        .required("Please enter a valid email"),
+        password: Yup.string()
+        .required("Please enter a password"),
+        organization: Yup.string()
+        .required("Organization name is required"),
+        contactFirst: Yup.string()
+        .required("Contact First Name is required"),
+        contactLast: Yup.string()
+        .required("Contact Last Name is required"),
+    })
+
     if(props.signIn === true){
-        return(
-        <div className="formSignIn">
-            <Form onSubmit={handleSubmit}>
-                <Input name="email" type="email" label="Email" placeholder="sammyGives@gmail.com"/>
-                <Input name="password" type="password" label="Password"/>
-                <button type="submit">Sign In</button>
+        return (
+          <div className="formSignIn">
+            <Form onSubmit={handleSubmit} schema={schema}>
+              <Input
+                name="email"
+                type="email"
+                label="Email"
+                placeholder="sammyGives@gmail.com"
+              />
+              <Input name="password" type="password" label="Password" />
+              <p>*All fields are required</p>
+              <button type="submit">Sign In</button>
             </Form>
-        </div>
-        )
+          </div>
+        );
     } else if (props.register === true){
-        return(
-            <div className="formRegister">
-                <Form onSubmit={handleSubmit}>
-                    <Input name="organization" type="text" label="Name of Organization" placeholder="Feed The Hungry, LLC"/>
-                    <Input name="contactFirst" type="text" label="Contact Name" placeholder="John"/>
-                    <Input name="contactLast" type="text" placeholder="Cross"/>
-                    <Input name="phone" type="text" label="Phone Number" placeholder="804-229-6345"/>
-                    <Input name="streetNumber" type="text" label="Address" placeholder="14233"/>
-                    <Input name="streetName" type="text" placeholder="Ocean Lane"/>
-                    <br/>
-                    <Input name="city" type="text" placeholder="Tuckahoe"/>
-                    <Input name="state" type="text" placeholder="VA"/>
-                    <br/>
-                    <Input name="zipcode" type="text" placeholder="23542"/>
-                </Form>
-            </div>
-        )
+        return (
+          <div className="formRegister">
+            <Form onSubmit={handleSubmit} schema={schema}>
+              <Input
+                name="organization"
+                type="text"
+                label="Name of Organization"
+                placeholder="Feed The Hungry, LLC"
+              />
+              <Input
+                name="contactFirst"
+                type="text"
+                label="Contact Name"
+                placeholder="John"
+              />
+              <Input
+                name="contactLast"
+                type="text"
+                placeholder="Cross"
+              />
+              <Input
+                name="phone"
+                type="text"
+                label="Phone Number"
+                placeholder="804-229-6345"
+              />
+              <Input
+                name="streetNumber"
+                type="text"
+                label="Address"
+                placeholder="14233"
+              />
+              <Input
+                name="streetName"
+                type="text"
+                placeholder="Ocean Lane"
+              />
+              <br />
+              <Input name="city" type="text" placeholder="Tuckahoe" />
+              <Input name="state" type="text" placeholder="VA" />
+              <br />
+              <Input name="zipcode" type="text" placeholder="23542" />
+              <p>*All fields are required</p>
+              <button type="submit">Register</button>
+            </Form>
+          </div>
+        );
     }else {
         return(
             <div></div>
