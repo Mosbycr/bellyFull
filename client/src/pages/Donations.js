5c7a2972ff305eedc6844fc3fc3d173b55ef2449
levinemr2 <levinemr2@vcu.edu>
2019-07-09 21:43:38 -0400

    methods, added api calls

--- client/src/pages/Donations.js
+++ client/src/pages/Donations.js
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import DonationsCard from "../components/DonationsCard/DonationsCard";
 import ClaimedCard from "../components/ClaimedCard/ClaimedCard";
+import API from "../utils/API";
+import List from "../components/List/List"
 
-function Donations (){
-  return(
-    <div className= "container-fluid">
-     <div className= "row">
-       <div className="col-md-6 text-center">
-         <h2>Available Donations</h2>
-         <hr></hr>
-         <br></br>
-         <DonationsCard/>
-       </div>
-       <div className = "col-md-6 text-center">
-         <h2>Claimed</h2>
-         <hr></hr>
-         <br></br>
-         <ClaimedCard/>
-       </div>
-    </div>
-    </div>
-  )
+
+class Donations extends Component {
+  state = {
+    food: [],
+    restaurant: "",
+    phone: "",
+    donations: ""
+  };
+
+  componentDidMount() {
+    this.loadFood();
+  }
+
+  loadFood = () => {
+    API.getFood()
+    .then(res => 
+      this.setState({ books: res.data, restaurant: "", phone: "", donations: ""})
+    )
+    .catch(err => console.log(err));
+  };
+
+  render() {
+    return(
+      <div className= "container-fluid">
+       <div className= "row">
+         <div className="col-md-6 text-center">
+           <h2>Available Donations</h2>
+           <hr></hr>
+           <br></br>
+           <List>
+
+           </List>
+           <DonationsCard/>
+         </div>
+         <div className = "col-md-6 text-center">
+           <h2>Claimed</h2>
+           <hr></hr>
+           <br></br>
+           <ClaimedCard/>
+         </div>
+      </div>
+      </div>
+    )
+  }
 }
+// function Donations (){
+//   return(
+//     <div className= "container-fluid">
+//      <div className= "row">
+//        <div className="col-md-6 text-center">
+//          <h2>Available Donations</h2>
+//          <hr></hr>
+//          <br></br>
+//          <DonationsCard/>
+//        </div>
+//        <div className = "col-md-6 text-center">
+//          <h2>Claimed</h2>
+//          <hr></hr>
+//          <br></br>
+//          <ClaimedCard/>
+//        </div>
+//     </div>
+//     </div>
+//   )
+// }
+
 
 export default Donations;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,15 +6,15 @@ export default {
     return axios.get("/api/food");
   },
   // Gets the book with the given id
-  getFood: function(id) {
+  getFoodById: function(id) {
     return axios.get("/api/food/" + id);
   },
   // Deletes the book with the given id
-  deleteFood: function(id) {
-    return axios.delete("/api/food/" + id);
-  },
-  // Saves a book to the database
-  saveFood: function(bookData) {
-    return axios.post("/api/food", bookData);
-  }
+  // deleteFood: function(id) {
+  //   return axios.delete("/api/food/" + id);
+  // },
+  // // Saves a book to the database
+  // saveFood: function(bookData) {
+  //   return axios.post("/api/food", bookData);
+  // }
 };
