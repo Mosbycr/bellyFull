0ee7edda662a03410f1fb0560cdf4e4de6a4ad9c
levinemr2 <levinemr2@vcu.edu>
2019-07-11 19:07:08 -0400

    looped donations to donations cards, added props

--- client/src/components/DonationsCard/DonationsCard.js
+++ client/src/components/DonationsCard/DonationsCard.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./style.css";
 import ClaimBtn from "../ClaimBtn/ClaimBtn";
 
-function DonationsCard(){
+
+function DonationsCard(props){
+
     return(
     <div className="card mx-auto" >
          <div className="card-header">
-         <h5 className="card-title">Restaurant Name</h5>
+
+         <h5>{props.restaurant}</h5>
+        {/* <h5 className="card-title">Restaurant Name</h5> */}
          </div>
         <ul className="list-group list-group-flush">
-            <li className="list-group-item">List of Items</li>
-            <li className="list-group-item">Contact info</li>
+            <li className="list-group-item">{props.listItems}</li>
+            <li className="list-group-item">{props.contact}</li>
        </ul>
             <div className="card-body">
               <ClaimBtn/>        
