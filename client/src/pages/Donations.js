fa9bf8aebe679ee131dfcd5a820639b4e3c19135
levinemr2 <levinemr2@vcu.edu>
2019-07-11 17:51:48 -0400

    rendering food from seed to donations card

--- client/src/pages/Donations.js
+++ client/src/pages/Donations.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 import DonationsCard from "../components/DonationsCard/DonationsCard";
 import ClaimedCard from "../components/ClaimedCard/ClaimedCard";
 import API from "../utils/API";
-import List from "../components/List/List"
+import List from "../components/List"
+//import { Container, Row, Col} from "../components/Container/container";
+//import Jumbotron from "../components/Jumbotron/Jumbotron";
+import ListItem from "../components/List";
+import { Link } from "react-router-dom";
+
 
 
 class Donations extends Component {
@@ -20,55 +25,81 @@ class Donations extends Component {
   loadFood = () => {
     API.getFood()
     .then(res => 
-      this.setState({ books: res.data, restaurant: "", phone: "", donations: ""})
+      this.setState({ food: res.data, restaurant: "", phone: "", donations: ""})
     )
     .catch(err => console.log(err));
   };
 
-  render() {
-    return(
-      <div className= "container-fluid">
-       <div className= "row">
-         <div className="col-md-6 text-center">
-           <h2>Available Donations</h2>
-           <hr></hr>
-           <br></br>
-           <List>
-
-           </List>
-           <DonationsCard/>
-         </div>
-         <div className = "col-md-6 text-center">
-           <h2>Claimed</h2>
-           <hr></hr>
-           <br></br>
-           <ClaimedCard/>
-         </div>
-      </div>
-      </div>
-    )
-  }
+  // render() {
+  //   return(
+  //     <Container fluid>
+  //     <Row>
+  //       <Col size="md-6">
+  //     <Jumbotron>
+  //             <h1>Books On My List</h1>
+  //           </Jumbotron>
+  //           {this.state.food.length ? (
+  //             <List>
+  //               {this.state.food.map(food => (
+  //                 <ListItem key={food._id}>
+  //                   <Link to={"/food/" + food._id}>
+  //                     <strong>
+  //                       {food.restaurant} by {food.phone}
+  //                     </strong>
+  //                     </Link>
+  //                 </ListItem>
+  //               ))}
+  //             </List>
+  //           ) : (
+  //             <h3>No Results to Display</h3>
+  //           )}
+  //         </Col>
+  //       </Row>
+  //     </Container>
+  //      );
+  //     }
+  //   }
+      
+  //  export default Donations;
+  //function Donations (){
+render() {
+  return(
+    
+    <div className= "container-fluid">
+     <div className= "row">
+       <div className="col-md-6 text-center">
+         <h2>Available Donations</h2>
+         <hr></hr>
+         <br></br>
+{this.state.food.length ? (
+  <List>
+    {this.state.food.map(food => (
+      <ListItem key={food._id}>
+        <Link to={"/food/" + food._id}>
+          <strong>
+            {food.restaurant} by {food.phone}
+          </strong>
+          </Link>
+      </ListItem>
+    ))}
+  </List>
+) : (
+  <h3>No Results to Display</h3>
+)}
+         <DonationsCard/>
+       </div>
+       <div className = "col-md-6 text-center">
+         <h2>Claimed</h2>
+         <hr></hr>
+         <br></br>
+         <ClaimedCard/>
+       </div>
+    </div>
+    </div>
+  )
 }
-// function Donations (){
-//   return(
-//     <div className= "container-fluid">
-//      <div className= "row">
-//        <div className="col-md-6 text-center">
-//          <h2>Available Donations</h2>
-//          <hr></hr>
-//          <br></br>
-//          <DonationsCard/>
-//        </div>
-//        <div className = "col-md-6 text-center">
-//          <h2>Claimed</h2>
-//          <hr></hr>
-//          <br></br>
-//          <ClaimedCard/>
-//        </div>
-//     </div>
-//     </div>
-//   )
-// }
-
-
+}
 export default Donations;
+
+
+
