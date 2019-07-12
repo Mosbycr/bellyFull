4c6d9e2127a53b029669625b29d0edb256f49e1c
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-07-11 21:01:37 -0400

    fixed footer

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,73 +4,75 @@ import Stats from "../components/Stats/Stats";
 function About() {
   return (
     <div className="container-fluid">
-        <div className= "about">
+      <div className="about">
         <div className="row justify-content-center">
-        <h1 className="aboutUs  mb-5 ">About Us</h1>
+          <h1 className="aboutUs  mb-5 ">About Us</h1>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="row">
+              <h3 className="font-weight-bold font-italic">Who we are... </h3>
+            </div>
+            <div className="row">
+              <div className="col-md-10">
+                <p>
+                  Belly Full was founded in Richmond, VA by Carrie Mosby, Nimeh
+                  Jadallah, and Molly Levine in 2019.
+                </p>
+              </div>
+            </div>
+
+            <div className="row">
+              <h3 className="font-weight-bold font-italic">Our Mission...</h3>
+            </div>
+            <div className="row">
+              <div className="col-md-10">
+                <p>
+                  We help restaurants connect with local non-profit
+                  organizations to donate their leftover food and reduce food
+                  waste.
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <h3 className="font-weight-bold font-italic">Our Vission...</h3>
+            </div>
+            <div className="row">
+              <div className="col-md-10">
+                <p>
+                  {" "}
+                  The US is facing a food waste crisis. Up to 40% of all food
+                  goes to waste across the country, and restaurants alone throw
+                  away 133 billion pounds of food each year.
+                </p>
+                <p>
+                  This has a serious impact on small business, food insecurity,
+                  and is a major contributing force to climate change.
+                </p>
+                <p>
+                  We seek a world of hope, tolerance and social justice, where
+                  poverty has been overcome and all people live with dignity and
+                  security.{" "}
+                  <a href="/LogIn" className="btn joinUs">
+                    Join Us Today
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-6">
+            <div className="row">
+              <div className="col-md-10">
+                <h3 className="hunger">What Hunger Looks Like in Virgina</h3>
+              </div>
+            </div>
+            <div> 
+              <Stats />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="row">
-            <h3 className="font-weight-bold font-italic">Who we are... </h3>
-          </div>
-          <div className="row">
-            <div className="col-md-9">
-              <p>
-                Belly Full was founded in Richmond, VA by Carrie Mosby, Nimeh
-                Jadallah, and Molly Levine in 2019.
-              </p>
-            </div>
-          </div>
-
-          <div className="row">
-            <h3 className="font-weight-bold font-italic">Our Mission...</h3>
-          </div>
-          <div className="row">
-            <div className="col-md-9">
-              <p>
-                We help restaurants connect with local non-profit organizations
-                to donate their leftover food and reduce food waste.
-              </p>
-            </div>
-          </div>
-          <div className="row">
-            <h3 className="font-weight-bold font-italic">Our Vission...</h3>
-          </div>
-          <div className="row">
-            <div className="col-md-9">
-              <p>
-                {" "}
-                The US is facing a food waste crisis. Up to 40% of all food goes
-                to waste across the country, and restaurants alone throw away
-                133 billion pounds of food each year.
-              </p>
-              <p>
-                This has a serious impact on small business, food insecurity,
-                and is a major contributing force to climate change.
-              </p>
-              <p>
-                We seek a world of hope, tolerance and social justice, where
-                poverty has been overcome and all people live with dignity and
-                security. <a href="/LogIn" className= "btn joinUs">Join Us Today</a>
-
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-6">
-          <div className="row justify-content-center">
-            <div className="col-md-9">
-              <h3 className="hunger">What Hunger Looks Like in Virgina</h3>
-            </div>
-          </div>
-          <div>
-            <Stats />
-          </div>
-        </div>
-      </div>
-        </div>
-      
     </div>
   );
 }
